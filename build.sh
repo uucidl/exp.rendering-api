6aec69fbf6dc78c126a3c7bc43d0292134d62876
Nicolas Léveillé <nicolas@uucidl.com>
2014-08-17 18:47:26 +0200

    point at correct build directories

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 HERE=$(dirname ${0})
-BUILD="${HERE}/build"
+BUILD="${HERE}/builds"
 [ -d "${BUILD}" ] || mkdir -p "${BUILD}"
 
 modules/uu.micros/build --src-dir "${HERE}/src" --output-dir "${BUILD}"
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 HERE=$(dirname ${0})
-BUILD="${HERE}/build"
+BUILD="${HERE}/builds"
 [ -d "${BUILD}" ] || mkdir -p "${BUILD}"
 
 "${HERE}"/build.sh && builds/$(hostname)/main
