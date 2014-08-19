31792706c16c873997e9ced7e971c0f1eff8368c
Nicolas Léveillé <nicolas@uucidl.com>
2014-08-19 23:55:12 +0200

    passing other parameters to build system

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ HERE="$(dirname ${0})"
 BUILD="${HERE}/builds"
 [ -d "${BUILD}" ] || mkdir -p "${BUILD}"
 
-"${HERE}"/modules/uu.micros/build --src-dir "${HERE}/src" --output-dir "${BUILD}"
+"${HERE}"/modules/uu.micros/build --src-dir "${HERE}/src" --output-dir "${BUILD}" "$@"
