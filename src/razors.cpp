b549de9aa9bf9176f4a87f90985c4dae359d03b7
Nicolas Léveillé <nicolas@uucidl.com>
2014-08-26 21:21:23 +0200

    macros to disable warnings on msvc as well

--- src/razors.cpp
+++ src/razors.cpp
@@ -4,12 +4,23 @@
 #include "glresource_types.hpp"
 #include "gldebug.hpp"
 
-#define BEGIN_NOWARN_BLOCK \
+#if defined(__clang__)
+#  define BEGIN_NOWARN_BLOCK \
         _Pragma("clang diagnostic push") \
         _Pragma("clang diagnostic ignored \"-Wmissing-field-initializers\"")
 
-#define END_NOWARN_BLOCK \
+#  define END_NOWARN_BLOCK \
         _Pragma("clang diagnostic pop")
+#elif defined(_MSC_VER)
+#  define BEGIN_NOWARN_BLOCK \
+        __pragma(warning(push, 3))
+
+#  define END_NOWARN_BLOCK \
+        __pragma(warning(pop))
+#else
+#  warning "missing definitions for BEGIN/END NOWARN BLOCKs"
+#endif
+
 
 BEGIN_NOWARN_BLOCK
 
