4b520a0ed38c046eb21c0e893074f3bbb765e553
Nicolas Léveillé <nicolas@uucidl.com>
2014-09-17 22:40:03 +0200

    allow generating 3d textures

--- src/gltexturing.cpp
+++ src/gltexturing.cpp
@@ -1,19 +1,24 @@
+#include "gltexturing.hpp"
+
 #include <GL/glew.h>
 
 #include <cstdint>
 #include <functional>
 #include <vector>
 
+// pixels are layed out in rows of width pixels from 0 to height
 void defineNonMipmappedARGB32Texture(
         int const width, int const height,
         std::function<void(uint32_t* pixels, int width, int height)> pixelFiller)
 {
+        auto const target = GL_TEXTURE_2D;
+
         // no mipmapping
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
         if (!pixelFiller) {
-                glTexImage2D(GL_TEXTURE_2D,
+                glTexImage2D(target,
                              0,
                              GL_RGBA,
                              width,
@@ -29,7 +34,7 @@ void defineNonMipmappedARGB32Texture(
 
         pixelFiller(&pixels.front(), width, height);
 
-        glTexImage2D(GL_TEXTURE_2D,
+        glTexImage2D(target,
                      0,
                      GL_RGBA,
                      width,
@@ -40,3 +45,49 @@ void defineNonMipmappedARGB32Texture(
                      &pixels.front());
 }
 
+/**
+ * call while a texture is bound to define a non mipmapped 3d texture
+ *
+ * pixels are layed out in layers from 0 to depth
+ */
+void defineNonMipmappedARG32Texture3d(int const width,
+                                      int const height,
+                                      int const depth,
+                                      std::function<void(uint32_t* pixels, int width, int height, int depth)>
+                                      pixelFiller)
+{
+        auto const target = GL_TEXTURE_3D;
+
+        // no mipmapping
+        glTexParameteri(target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        if (!pixelFiller) {
+                glTexImage3D(GL_TEXTURE_2D,
+                             0,
+                             GL_RGBA,
+                             width,
+                             height,
+                             depth,
+                             0,
+                             GL_RGBA,
+                             GL_UNSIGNED_INT_8_8_8_8_REV,
+                             NULL);
+                return;
+        }
+
+        std::vector<uint32_t> pixels (width * height * depth);
+
+        pixelFiller(&pixels.front(), width, height, depth);
+
+        glTexImage3D(target,
+                     0,
+                     GL_RGBA,
+                     width,
+                     height,
+                     depth,
+                     0,
+                     GL_RGBA,
+                     GL_UNSIGNED_INT_8_8_8_8_REV,
+                     &pixels.front());
+}
--- src/gltexturing.hpp
+++ src/gltexturing.hpp
@@ -3,7 +3,22 @@
 #include <cstdint>
 #include <functional>
 
-/// call while a texture bound to define a non mipmapped 2d texture
+/**
+ * call while a texture bound to define a non mipmapped 2d texture
+ *
+ * pixels are layed out in rows of width pixels from 0 to height
+ */
 void defineNonMipmappedARGB32Texture(
         int const width, int const height,
         std::function<void(uint32_t* pixels, int width, int height)> pixelFiller);
+
+/**
+ * call while a texture is bound to define a non mipmapped 3d texture
+ *
+ * pixels are layed out in layers from 0 to depth
+ */
+void defineNonMipmappedARG32Texture3d(int const width,
+                                      int const height,
+                                      int const depth,
+                                      std::function<void(uint32_t* pixels, int width, int height, int depth)>
+                                      pixelFiller);
