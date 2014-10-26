2b079cee29f2a71eff2e84514be49e1d9d556cbb
Nicolas Léveillé <nicolas@uucidl.com>
2014-10-26 14:04:07 +0100

    framebuffers are now necessary for renderer lib

--- gl3texture/libs.cpp
+++ gl3texture/libs.cpp
@@ -1,9 +1,10 @@
 // implementations
 
-#include "../ref/fs.cpp"
+#include "../gl3companion/glframebuffers.cpp"
 #include "../gl3companion/glresources.cpp"
 #include "../gl3companion/glshaders.cpp"
 #include "../gl3companion/gltexturing.cpp"
+#include "../ref/fs.cpp"
 
 #define STB_PERLIN_IMPLEMENTATION
 #include "stb_perlin.h"
